4884f4d1e48dd0e6d0f300d48db53631c3b1ba28
Vonavy <ivanov070901@yandex.ru>
2022-10-07 19:31:17 +1000

    Set css to "latest"

--- dist/index.js
+++ dist/index.js
@@ -576,7 +576,7 @@ var nkchCSS = /** @class */ (function () {
 }());
 jQuery(function () {
     var options = {};
-    mw.loader.load("https://nkch.fandom.com/wiki/MediaWiki:NkchCSS4.css?action=raw&ctype=text/css", "text/css");
+    mw.loader.load("https://cdn.jsdelivr.net/gh/Vonavy/nkch-css@latest/css/index.css", "text/css");
     if (window.nkch) {
         if (window.nkch.css4)
             return;
--- src/index.ts
+++ src/index.ts
@@ -800,7 +800,7 @@ class nkchCSS {
 jQuery(() => {
     let options: nkch.css.Options = {};
 
-    mw.loader.load("https://cdn.jsdelivr.net/gh/Vonavy/nkch-css/css/index.css", "text/css");
+    mw.loader.load("https://cdn.jsdelivr.net/gh/Vonavy/nkch-css@latest/css/index.css", "text/css");
 
     if (window.nkch) {
         if (window.nkch.css4) return;
